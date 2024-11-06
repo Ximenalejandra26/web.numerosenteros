d2591a4fc23531a70d4cbc5cacc1f5e4419f105c
ximena <ximena.aleja021@outlook.com>
2024-11-05 20:48:39 -0500

    commit inicial

---
+++ script.js
@@ -0,0 +1,6 @@
+//asignamos 0 a las variables negativos, positivos y multiplosDe15,
+// esto es para asegurarnos de que el contador esté vacío al comienzo del programa.
+let negativos = 0;
+let positivos = 0;
+let multiplosDe15 = 0;
+let valor;
