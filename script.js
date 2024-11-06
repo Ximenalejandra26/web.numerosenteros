d7e60f5aa918b680da1d37d508563624a3c71683
ximena <ximena.aleja021@outlook.com>
2024-11-05 20:53:13 -0500

    uso el console.log para que me muestre la informacion en la consola

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ let valor;
 //Se ejecuta 5 veces, solicitando al usuario que ingrese un valor entero en cada iteración
 for (let i = 0; i < 5; i++) {
     valor = parseInt(prompt('Ingresa un valor entero:'));
-}
 
     // Se usa if para verificar si (valor < 0) el valor es negativo mientras que, (valor > 0), el valor es positivo 
     if (valor < 0) {
@@ -16,3 +15,12 @@ for (let i = 0; i < 5; i++) {
     } else if (valor > 0) { 
         positivos++;
     }
+    if (valor % 15 === 0) {
+        multiplosDe15++;
+    }
+}
+
+// uso el console.log para que me muestre la inf en la consola
+console.log(`Cantidad de valores negativos ingresados: ${negativos}`);
+console.log(`Cantidad de valores positivos ingresados: ${positivos}`);
+console.log(`Cantidad de múltiplos de 15: ${multiplosDe15}`);
